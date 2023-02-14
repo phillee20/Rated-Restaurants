f36d7b67e67af53b0d1506dd18479c828074de11
Helen Morris <morris_helen@hotmail.com>
2023-02-14 16:43:57 +0000

    complete post function

--- db/controller.js
+++ db/controller.js
@@ -1,10 +1,17 @@
-const fetchRestaurants = require("./models");
+const {fetchRestaurants, insertRestaurant} = require("./models");
 
 const getRestaurants = (request, response) => {
-  //console.log(request.params);
   fetchRestaurants().then((restaurants) => {
     response.status(200).send({ restaurants });
   });
 };
 
-module.exports = getRestaurants;
+const postRestaurant = (request, response) => {
+  const { restaurant_name, area_id, cuisine, website } = request.body;
+  
+  insertRestaurant(restaurant_name, area_id, cuisine, website).then((restaurant) => {
+    response.status(201).send({ restaurant });
+  })
+}
+
+module.exports = {getRestaurants, postRestaurant};
