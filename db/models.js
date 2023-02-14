0b4133fb231728e5a7bc87303ea77860bd0099d4
Helen Morris <morris_helen@hotmail.com>
2023-02-14 16:44:15 +0000

    complete post function

--- db/models.js
+++ db/models.js
@@ -2,9 +2,21 @@ const db = require("./connection");
 
 const fetchRestaurants = () => {
   return db.query(`SELECT * FROM restaurants;`).then((restaurantsData) => {
-    //console.log(restaurantsData.rows);
     return restaurantsData.rows;
   });
 };
 
-module.exports = fetchRestaurants;
+const insertRestaurant = (restaurant_name, area_id, cuisine, website) => {
+
+  return db.query(`
+    INSERT INTO restaurants 
+      (restaurant_name, area_id, cuisine, website)
+      VALUES
+      ($1, $2, $3, $4)
+    RETURNING *;`, [restaurant_name, area_id, cuisine, website])
+    .then(({ rows }) => {
+      return rows[0];
+    })
+}
+
+module.exports = {fetchRestaurants, insertRestaurant};
